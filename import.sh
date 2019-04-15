bab19185979f5b50ab581eb7a0790da6cc26ccee
ilan schemoul <xd111@hotmail.fr>
2019-04-15 20:07:50 +0200

    SB

--- import.sh
+++ import.sh
@@ -1,24 +1,28 @@
 #!/bin/bash 
 
+cp ~/.bashrc ./configs/.bashrc
+cp ~/.xinitrc ./configs/.xinitrc
+cp ~/.Xresources ./configs/.Xresources
+cp ~/.xbindkeysrc ./configs/.xbindkeysrc
+cp ~/.config/.chromium-flags.conf ~/configs/.config/.chromium-flags.conf
+cp ~/.config/htop ~/configs/.config/htop
+cp ~/.config/libinput-gestures.conf ~/configs/.config/libinput-gestures.conf
+cp -R ~/.config/polybar ~/configs/.config/polybar
+cp ~/.config/redshift/redshift.conf ~/configs/.config/redshift/redshift.conf
+cp ~/.config/rofi/config ~/configs/.config/rofi/config
+cp ~/.config/touchegg/touchegg.conf ~/configs/.config/touchegg/touchegg.conf
 cp ~/.vimrc ./configs/.vimrc
 cp ~/.config/i3/config ./configs/.config/i3/config 
 cp ~/.compton.conf ./configs/.compton.conf 
 
+
+# Gentoo nethack
 if [ -e ~/.nethackrc ]; then
     cp ~/.nethackrc ./configs/nethackrc
 fi
 
-cp ~/.Xresources ./configs/.Xresources
-cp ~/.bashrc ./configs/.bashrc
-cp ~/.xbindkeysrc ./configs/.xbindkeysrc 
-cp ~/.xinitrc ./configs/.xinitrc
-
 if [ -e /etc/nethackrc ]; then
     cp /etc/nethackrc ./configs/.nethackrc
-fi
-
-if [ -e ~/.nethackrc ]; then
-    cp ~/.nethackrc ./config/nethackrc
 fi
 
 if [ -e /usr/games/nethack/NetHack.ad ]; then
--- install.sh
+++ install.sh
@@ -4,16 +4,16 @@ echo "Are you sure that you want to install new configs ? (y/n)"
 read answer
 
 if [[ "$answer" =~ [Yy] ]]; then
-    if [ -e ~/.config/i3/config ]
-    then
-        cp -r ./configs/.config/i3/config ~/.config/i3/config
-        echo "i3 config installed"
-    else
-        echo ".config/i3/config not found"
-    fi
+    #if [ -e ~/.config/i3/config ]
+    #then
+    #    cp -r ./configs/.config/i3/config ~/.config/i3/config
+    #    echo "i3 config installed"
+    #else
+    #    echo ".config/i3/config not found"
+    #fi
 
-    cp ./configs/.vimrc ~/.vimrc
-    echo ".vimrc installed"
+    cp ./configs/. ~
+    echo "config files imported"
 
     if [ -x "$(command -v git)" ] && [ -x "$(command -v vim)" ]; then
         if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
@@ -22,7 +22,7 @@ if [[ "$answer" =~ [Yy] ]]; then
         fi
         vim +PluginInstall +qall
     else
-        echo "install git first then run this again."
+        echo "install git and vim then run this again."
         exit 1
     fi
 
